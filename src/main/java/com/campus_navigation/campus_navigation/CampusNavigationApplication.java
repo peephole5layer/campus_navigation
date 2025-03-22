c1952798f63c4c449ce3723d3fc3ec7dc5079cd8
peephole5layer <divyanshu002mishra@gmail.com>
2025-03-22 09:42:22 +0530

    Initial commit

---
+++ src/main/java/com/campus_navigation/campus_navigation/CampusNavigationApplication.java
@@ -0,0 +1,13 @@
+package com.campus_navigation.campus_navigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusNavigationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusNavigationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campus_navigation/campus_navigation/CampusNavigationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus_navigation.campus_navigation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusNavigationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
